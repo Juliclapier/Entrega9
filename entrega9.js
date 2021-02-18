1f57b34fbecfa08baf28d18e405896597ce716bf
juliana <julianaclapier@gmail.com>
2021-02-18 12:51:31 -0300

    .

--- entrega9.js
+++ entrega9.js
@@ -1,49 +1,21 @@
 
-  function mostrar() {
-      var inputUno = document.getElementById("nombre").value.toLowerCase();
-    var inputDos = document.getElementById("pass").value.toLowerCase();
-    
-    
-    
-    
-    boton.addEventListener('click', mostrar())
+var inputUno = document.getElementById("nombre")
+var inputDos = document.getElementById("pass")
 
-       if(inputDos.value === 'clapier'){
-    
-            console.log('el campo contraseña es obligatorio')
-            evt.preventDefault();
-            return false;
-    
-        }
-        else if(inputUno.value === 'juliana'){
-    
-        console.log('el campo de usuario es obligatorio')
-            evt.preventDefault();
-            return false;
-    
-        }
-        else if(inputUno.value.length > 30){
-    
-          console.log('El nombre del usuario es demasiado largo')
-            evt.preventDefault();
-            return false;
-        }
-     if(inputUno.value = true) {
-         document.getElementById("saludo").innerHTML= "Usuario Correcto"
+  function mostrar(inputUno,inputDos) {
+      
+
+    return (inputDos.value, inputUno.value)
+  }
+
+    function mostrarDos (){document.addEventListener('click',mostrarDos)
+
+       if(inputDos.value != 'clapier' && inputUno.vale !='juliana'){
+        document.getElementById("saludo").innerHTML= "Usuario incorrecto"
       }
-    else {document.getElementById("saludo").innerHTML= "Usuario incorrecto"}}
-
+    else {document.getElementById("saludo").innerHTML= "Usuario correcto"}}
 
     
-// document
-// .getElementById("boton-toto")
-// .addEventListener("click", saludarALosCoders);
 
-// function saludarALosCoders(event) {
-// if(event.target.value.length < 4){
-//   alert("La contraseña debe tener mas de 3 caracteres");
-// }
-// console.log("funciona");
-// document
-//   .getElementById("boton-toto")
-//   .removeEventListener("click", saludarALosCoders, false);
+
+ 
